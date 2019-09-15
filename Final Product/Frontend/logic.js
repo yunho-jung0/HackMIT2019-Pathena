f2fc21f5f7ba5ad81c1fbbf9e9a2f5f7e2bb4bfa
Dolcevice <Dolcevice@users.noreply.github.com>
2019-09-15 11:01:36 -0400

    Update logic.js

--- Final Product/Frontend/logic.js
+++ Final Product/Frontend/logic.js
@@ -1,7 +1,17 @@
 var map;
-var markers = [];
-var mlen = 0;
+
+var start_marker = null;
+var end_marker = null;
+
 var which = 1;
+
+var start_point_x;
+var start_point_y;
+var end_point_x;
+var end_point_y;
+
+var start_pa = 1;
+var end_pa = 1;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -10,36 +20,39 @@ function initMap() {
     });
 
     map.addListener("click", function(ev){
-        var mtitle;
-        if (which == 1) {
-            mtitle = "Start";
-        }
-        else {
-            mtitle = "End";
-        }
         var m = new google.maps.Marker({
             position: ev.latLng,
             map: map,
-            title: mtitle
+            title: "REEE"
         });
 
-        if (markers.length < 2) {
-            markers[markers.length] = m;
+        var block = false;
+        if (start_pa == 1) {
+            if (which == 1 || end_pa == 2) {
+                console.log("start block");
+                if (start_marker) {
+                    start_marker.setMap(null);
+                }
+                m.setTitle("Start");
+                start_marker = m;
+                start_point_x = ev.latLng.lng();
+                start_point_y = ev.latLng.lat();
+                which = 2;
+                block = true;
+            }
         }
-        else {
-            markers[which-1].setMap(null);
-            markers[which-1] = m;
-        }
-
-        if (which == 1) {
-            $("#p1x").val(ev.latLng.lng());
-            $("#p1y").val(ev.latLng.lat());
-            which = 2;
-        }
-        else {
-            $("#p2x").val(ev.latLng.lng());
-            $("#p2y").val(ev.latLng.lat());
-            which = 1;
+        if (end_pa == 1 && !block) {
+            if (which == 2 || start_pa == 2) {
+                console.log("end block");
+                if (end_marker) {
+                    end_marker.setMap(null);
+                }
+                m.setTitle("End");
+                end_marker = m;
+                end_point_x = ev.latLng.lng();
+                end_point_y = ev.latLng.lat();
+                which = 1;
+            }
         }
             
         console.log(ev.latLng.lat());
@@ -50,6 +63,27 @@ function initMap() {
 var poly = null;
 
 $(document).ready(function(){
+    $("#sSP").click(function(){
+        start_pa = 1;
+        $("#addr0").val("");
+    });
+    $("#sSA").click(function(){
+        start_pa = 2;
+        if (start_marker) {
+            start_marker.setMap(null);
+        }
+    });
+    $("#sEP").click(function(){
+        end_pa = 1;
+        $("#addr1").val("");
+    });
+    $("#sEA").click(function(){
+        end_pa = 2;
+        if (end_marker) {
+            end_marker.setMap(null);
+        }
+    });
+
     $("#run").click(function(){
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "/route", true);
@@ -82,15 +116,36 @@ $(document).ready(function(){
             poly.setMap(map);
 
         };
+
+        var start_data;
+        if (start_pa == 1) {
+            start_data = {
+                type: "coordinate",
+                x: start_point_x,
+                y: start_point_y};
+        }
+        else {
+            start_data ={
+                type: "address",
+                address: $("#addr0").val()};
+        }
+
+        var end_data;
+        if (end_pa == 1) {
+            end_data = {
+                type: "coordinate",
+                x: end_point_x,
+                y: end_point_y};
+        }
+        else {
+            end_data = {
+                type: "address",
+                address: $("#addr1").val()};
+        }
+
         data = {
-            "start": {
-                type: "coordinate",
-                x: parseFloat($("#p1x").val()),
-                y: parseFloat($("#p1y").val())},
-            "end": {
-                type: "coordinate",
-                x: parseFloat($("#p2x").val()),
-                y: parseFloat($("#p2y").val())}};
+            start: start_data,
+            end: end_data};
         xhr.send(JSON.stringify(data));
     });
 });
